33a5bbce3d0bc4f8e9aec220a32a37b0c5ec6d43
Malte Rohde <malte.rohde@inf.fu-berlin.de>
2012-06-14 14:01:54 +0200

    ZBuffer: Allow setPixel out-of-range.

--- src/de/softwarekollektiv/cg/gl/ZBuffer.java
+++ src/de/softwarekollektiv/cg/gl/ZBuffer.java
@@ -31,6 +31,11 @@ class ZBuffer {
 	}
 
 	void setPixel(int x, int y, double z, final Vector3f c) {
+		if(x < 0 || y < 0 || x >= width || y >= height) {
+			System.out.println("Warning: Pixel out of range in ZBuffer.");
+			return;
+		}
+					
 		if (z < zindex[x][y]) {
 			zindex[x][y] = z;
 			pixel[x][y] = c;
